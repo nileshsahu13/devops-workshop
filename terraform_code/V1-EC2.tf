e975356724c762d62b270e50eb9c69f76a6bcee5
Nilesh Sahu <nilesh.sahu13@outlook.com>
2025-02-25 18:11:39 +0530

    commiting V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-05b10e08d247fb927"
     instance_type = "t2.micro"
     key_name = "dpp"
 }
